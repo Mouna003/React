d57078b06acb9acae6837e5a45bd0e978609f363
Mouna Majdi <145588548+Mouna003@users.noreply.github.com>
2025-03-16 15:39:47 +0100

    Add files via upload

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello Dojo</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Build Projects</li>
+        <li>Master JSX</li>
+        <li>Deploy a React App</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
